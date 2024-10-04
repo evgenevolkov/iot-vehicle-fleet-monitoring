3a4e9b70ddee903f8dc9a423bbe238c9bf7f6882
Evgene V <evgenevolkov@users.noreply.github.com>
2024-10-04 15:23:00 +0200

    add heading module

---
+++ vehicles_simulator/app/core/heading.py
@@ -0,0 +1,9 @@
+from app.core.interfaces import HeadingDirectionsInterface
+from app.utils import (
+    schemas,
+    )
+from app.utils.logger import get_logger
+
+logger = get_logger(__name__)
+
+
