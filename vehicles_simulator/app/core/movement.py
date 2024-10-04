bab1cdf0c70ed08840602a16f966aed9eb2de039
Evgene V <evgenevolkov@users.noreply.github.com>
2024-10-04 15:16:46 +0200

    add location module

---
+++ vehicles_simulator/app/core/movement.py
@@ -0,0 +1,10 @@
+from app.core.interfaces import (
+    MovementManager
+    )
+from app.utils import (
+    schemas,
+    )
+from app.utils.logger import get_logger
+
+logger = get_logger(__name__)
+
