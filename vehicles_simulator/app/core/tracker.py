dc1313f1e089937c9ca1e39759202b4c451d5a30
Evgene V <evgenevolkov@users.noreply.github.com>
2024-10-09 22:54:15 +0200

    implement collect_tracking_data method

--- vehicles_simulator/app/core/tracker.py
+++ vehicles_simulator/app/core/tracker.py
@@ -1,4 +1,5 @@
 """Contains implementation of BasicTrackerManager class."""
+from datetime import datetime, timezone
 from typing import Optional
 import random
 from uuid import uuid4, UUID
@@ -70,3 +71,49 @@ class BasicTrackerManager(TrackerManager):
         """Generates a new status value based on dict of status and
         probabilies of a value to be set."""
         self.current_status = self._generate_status()
+
+    def collect_tracking_data(self) -> None:
+        """Main function to collect all traking metrics"""
+        self.tracking_data = schemas.TrackingData(
+            task_state=self._get_task_state(),
+            vehicle_location=self._get_vehicle_location(),
+            destination=self._get_destination(),
+            vehicle_speed=self._get_vehicle_speed(),
+            heading_direction=self._get_heading_direction(),
+            distance_to_destination=self._get_distance_to_destination(),
+            out_of_zone_status=self._get_out_of_zone_status(),
+            created_time=self._get_current_time(),
+        )
+
+    def _get_task_state(self) -> str:
+        """Helper method to get task manager state"""
+        return self.tasks_manager.task_state.value
+
+    def _get_vehicle_location(self) -> schemas.Location:
+        """Helper method to get vehicle current location"""
+        return self.navigation_manager.current_location
+
+    def _get_destination(self) -> schemas.Location:
+        """Helper method to get current destination"""
+        return self.navigation_manager.destination
+
+    def _get_vehicle_speed(self) -> int:
+        """Helper method to get current speed"""
+        return self.navigation_manager.current_speed
+
+    def _get_heading_direction(self) -> schemas.Direction:
+        """Helper method to get current heading direction"""
+        return self.navigation_manager.current_direction
+
+    def _get_distance_to_destination(self) -> float:
+        """Helper method to get distance to destination"""
+        return self.navigation_manager.distance_to_destination
+
+    def _get_out_of_zone_status(self) -> bool:
+        """Helper method to get out of zone status"""
+        return self.navigation_manager.out_of_zone
+
+    def _get_current_time(self) -> datetime:
+        """Helper method to get current time"""
+        return datetime.now(timezone.utc).isoformat(timespec="milliseconds")
+
