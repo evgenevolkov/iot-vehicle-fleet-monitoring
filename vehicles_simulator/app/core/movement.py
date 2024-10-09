5a00668a50eed8d16fd6713c6046f4e0446eda72
Evgene V <evgenevolkov@users.noreply.github.com>
2024-10-10 00:18:12 +0200

    add docstrings

--- vehicles_simulator/app/core/movement.py
+++ vehicles_simulator/app/core/movement.py
@@ -51,14 +51,17 @@ class BasicMovementManager(MovementManager):
         self.shift: schemas.Shift
 
     def increase_speed(self) -> None:
+        """Increases vehicle speed by 1 if within max_speed limit"""
         self.current_speed = min(self.current_speed + 1, self.max_speed)
         logger.debug(f" Increased speed to {self.current_speed}")
 
     def decrease_speed(self) -> None:
+        """Decreases vehicle speed by 1 until 1"""
         self.current_speed = max(self.current_speed - 1, 1)
         logger.debug(f"Decreased speed to {self.current_speed}")
 
     def turn(self, direction: schemas.Direction) -> None:
+        """Try to make a turn or report why can't otherwise"""
         if self.can_turn:
             self.current_direction = direction
             self.distance_until_turn_allowed = \
@@ -97,6 +100,12 @@ class BasicMovementManager(MovementManager):
         return self.shift
 
     def _decide_speed_change(self) -> None:
+        """
+        Implements logic of speed change decision.
+
+        If distance value until next turn possibility is greater than
+        current speed, then increase speed. Decrease otherwise.
+        """
         logger.debug(f'Curr_speed: {self.current_speed}')
         if self.distance_until_turn_allowed > self.current_speed:
             self.increase_speed()
@@ -104,6 +113,7 @@ class BasicMovementManager(MovementManager):
             self.decrease_speed()
 
     def _get_move_shift(self) -> schemas.Shift:
+        """Calculate the move shift based on current speed."""
         single_shift = schemas.Movement[self.current_direction.name].value
         move_shift = schemas.Shift(
             x=single_shift[0] * self.current_speed,
@@ -111,9 +121,21 @@ class BasicMovementManager(MovementManager):
         return move_shift
 
     def _check_can_turn(self) -> None:
+
+        """
+        Check if a turn is allowed.
+
+        Both conditions a required be met:
+        1. Speed equals or less than threshold
+        2. Distance until turn allowed is 0
+        """
         self.can_turn = all([
             self.current_speed <= 1,
             self.distance_until_turn_allowed == 0])
 
     def _define_distance_until_turn(self) -> int:
+        """Simulate retrieveing distance until next turn.
+
+        Currently, for sample purpose, simulation formula is applied.
+        """
         return int(random.randint(1, 49) ** 0.5) + 2
